908f3cfb7600d779f129157a17bb0a14477a44d3
Jungdo Lee <lee.jungdo@gmail.com>
2014-11-02 02:41:32 +0900

    finish italy

--- italy.rb
+++ italy.rb
@@ -2,6 +2,7 @@ require "./importio.rb"
 require "json" 
 
 #################### PART 1 ####################
+# name: it_amazon_recent
 # Get most recent titles and their book page urls from Amazon Italy
 
 client = Importio::new("3f9ae37e-acfd-44f4-8157-e72adcc5b283","93CLLmP2bc/xrnSLz8b0BAsVyjebOMqgkxsEz/zmojXOtNoPd383KfJLaLXJqaaUzDY8bxZpfM5sDQKi4yUAxg==", "https://query.import.io")
@@ -9,6 +10,8 @@ client = Importio::new("3f9ae37e-acfd-44f4-8157-e72adcc5b283","93CLLmP2bc/xrnSLz
 client.connect
 
 data_rows_1 = []
+query_num = 0
+i = 0
 
 callback = lambda do |query, message|
   if message["type"] == "DISCONNECT"
@@ -19,24 +22,35 @@ callback = lambda do |query, message|
       puts "Got an error!"
       puts JSON.pretty_generate(message["data"])
       data_rows_1 << message["data"]["results"]
+      if data_rows_1[i].empty?
+        abort "\nFailed. Terminated."
+      end
   	else
       puts "Got data!"
       puts JSON.pretty_generate(message["data"])
       data_rows_1 << message["data"]["results"]
+      if data_rows_1[i].empty?
+        abort "\nWTF?! Failed at page #{i + 1}. Terminated.\n"
+      else
+        i += 1
+      end
     end
   end
   if query.finished
-    puts "Query finished"
+    query_num += 1
+    puts "Page #{query_num} finished"
   end
 end
 
-client.query({"input"=>{"webpage/url"=>"http://www.amazon.it/s/ref=sr_st_date-desc-rank?__mk_it_IT=%C3%85M%C3%85Z%C3%95%C3%91&unfiltered=1&qid=1414734670&rh=n%3A411663031%2Cn%3A!411664031%2Cn%3A508715031%2Cp_47%3A2014%2Cp_n_binding_browse-bin%3A509801031%2Cp_45%3A1%2Cp_46%3AAfter&sort=date-desc-rank"},"connectorGuids"=>["cbe042bf-7d40-4a98-b1f8-2c58a89b8e09"]}, callback )
+# Page 1
+# client.query({"input"=>{"webpage/url"=>"http://www.amazon.it/s/ref=sr_st_date-desc-rankhttp://www.amazon.it/s/ref=sr_adv_b?page_nav_name=Libri+in+italiano&search-alias=stripbooks&unfiltered=1&__mk_it_IT=%C5M%C5Z%D5%D1&field-keywords=&field-author=&field-title=&field-isbn=&field-publisher=&node=508715031&field-binding_browse-bin=&field-dateop=After&field-datemod=1&field-dateyear=2014&sort=-pubdate&Adv-Srch-Books-Submit.x=43&Adv-Srch-Books-Submit.y=8"},"connectorGuids"=>["cbe042bf-7d40-4a98-b1f8-2c58a89b8e09"]}, callback )
+sleep 2
 
 # Page 2 and above
-(2..3).each do |page|
-  client.query({"input"=>{"webpage/url"=>"http://www.amazon.it/s/ref=sr_pg_#{page}/280-7932322-1502062?rh=n%3A411663031%2Cn%3A%21411664031%2Cn%3A508715031%2Cp_47%3A2014%2Cp_n_binding_browse-bin%3A509801031%2Cp_45%3A1%2Cp_46%3AAfter&page=2&sort=date-desc-rank&unfiltered=1&ie=UTF8&qid=1414735761"},"connectorGuids"=>["cbe042bf-7d40-4a98-b1f8-2c58a89b8e09"]}, callback )
+(21..75).each do |page|
+  client.query({"input"=>{"webpage/url"=>"http://www.amazon.it/s/ref=sr_pg_#{page}?rh=n%3A411663031%2Cn%3A%21411664031%2Cn%3A508715031%2Cp_47%3A2014%2Cp_45%3A1%2Cp_46%3AAfter&page=#{page}&sort=-pubdate&unfiltered=1&ie=UTF8"},"connectorGuids"=>["cbe042bf-7d40-4a98-b1f8-2c58a89b8e09"]}, callback )
+  sleep 2
 end
-
 
 puts "Queries dispatched, now waiting for results"
 client.join
@@ -51,12 +65,6 @@ File.new('data/italy_part_1.json', 'w') unless File.exists?('data/italy_part_1.j
 # Open the file and write the data results to results_file.json
 File.open('data/italy_part_1.json', 'w') do |f|
   f << JSON.pretty_generate(data_rows_1)
-end
-
-i = 0
-until i == data_rows_1.size
-  puts "Page #{i}: #{data_rows_1[i].size} titles"
-  i += 1
 end
 
 # Get book page urls from data_rows_1
@@ -75,16 +83,30 @@ end
 puts book_page_urls
 puts book_page_urls.size
 
+i = 0
+until i == data_rows_1.size
+  puts "Page #{i + 1}: #{data_rows_1[i].size} titles"
+  i += 1
+end
+
+i = 0
+until i == data_rows_1.size
+  if data_rows_1[i].empty?
+    abort "\nFailed. Terminated."
+  end
+  i += 1
+end
+
+
+
 #################### PART 2 ####################
+# name: it_amazon_book_ext
 # With given book_page_urls, go to each page and extract data
-
-client = Importio::new("3f9ae37e-acfd-44f4-8157-e72adcc5b283","93CLLmP2bc/xrnSLz8b0BAsVyjebOMqgkxsEz/zmojXOtNoPd383KfJLaLXJqaaUzDY8bxZpfM5sDQKi4yUAxg==", "https://query.import.io")
-
+client = Importio::new("3f9ae37e-acfd-44f4-8157-e72adcc5b283","93CLLmP2bc/xrnSLz8b0BAsVyjebOMqgkxsEz/zmojXOtNoPd383KfJLaLXJqaaUzDY8bxZpfM5sDQKi4yUAxg==")
 client.connect
 
 data_rows_2 = []
 query_num = 0
-
 callback = lambda do |query, message|
   if message["type"] == "DISCONNECT"
     puts "The query was cancelled as the client was disconnected"
@@ -93,9 +115,7 @@ callback = lambda do |query, message|
     if message["data"].key?("errorType")
       puts "Got an error!"
       puts JSON.pretty_generate(message["data"])
-      data_rows_2 << message["data"]["results"]
     else
-
       puts "Got data!"
       puts JSON.pretty_generate(message["data"])
       data_rows_2 << message["data"]["results"]
@@ -103,13 +123,13 @@ callback = lambda do |query, message|
   end
   if query.finished
     query_num += 1
-    puts "Query #{query_num} finished"
+    puts "Book #{query_num} finished"
   end
 end
 
-# Query for tile it_amazon_ext
+# Query for tile it_amazon_book_ext
 book_page_urls.each do |url|
-  client.query({"input"=>{"webpage/url"=>url},"connectorGuids"=>["92e0ae4b-dc84-47ad-9ac0-c138ab460200"]}, callback )
+  client.query({"input"=>{"webpage/url"=>url},"connectorGuids"=>["320d7136-ea26-46e3-b3c1-eb7427993f8c"]}, callback )
 end
 
 puts "Queries dispatched, now waiting for results"

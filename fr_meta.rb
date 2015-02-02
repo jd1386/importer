80003329ad96a1249fa9a19e8ffcd77894e059d4
Jungdo Lee <lee.jungdo@gmail.com>
2015-02-02 20:07:03 +0900

    finish france module

---
+++ fr_meta.rb
@@ -0,0 +1,121 @@
+require "./importio.rb"
+require "json"
+require "csv"
+
+# Read and save book page urls
+book_page_urls = []
+File.readlines('data/fr_meta_source.txt').each do |line|
+  book_page_urls << line
+end
+
+book_page_urls_size = book_page_urls.size
+
+
+# Extract isbn from data/fr_meta_source.txt file
+isbns = []
+i = 0
+n = 0
+
+until i == book_page_urls_size  
+  isbns << book_page_urls[i].match(/978(?:-?\d){10}/)
+
+  i += 1
+end
+
+# Open the file and write the data results to results_file.json
+File.open('data/fr_meta_results_isbns.txt', 'w') do |f|
+  isbns.each { |isbn| f.puts(isbn) }
+end
+
+puts isbns
+puts "ISBNS size: #{isbns.size}"
+
+
+puts "Loaded book_page_urls size: #{book_page_urls_size} urls.
+		\nExtracted isbns from source file and saved them to data/fr_meta_results_isbns.txt.
+		\nNow starting to make queries..."
+puts "...in 3..." 
+sleep 1
+puts "...in 2..."
+sleep 1
+puts "...in 1..."
+sleep 1
+
+
+client = Importio::new("3f9ae37e-acfd-44f4-8157-e72adcc5b283","93CLLmP2bc/xrnSLz8b0BAsVyjebOMqgkxsEz/zmojXOtNoPd383KfJLaLXJqaaUzDY8bxZpfM5sDQKi4yUAxg==", "https://query.import.io")
+
+client.connect
+
+data_rows = []
+q = 0
+
+callback = lambda do |query, message|
+  if message["type"] == "DISCONNECT"
+    puts "The query was cancelled as the client was disconnected"
+  end
+  if message["type"] == "MESSAGE"
+    if message["data"].key?("errorType")
+      puts "Got an error!"
+      puts JSON.pretty_generate(message["data"])
+  	else
+      puts "Got data!"
+      puts JSON.pretty_generate(message["data"])
+      data_rows << message["data"]["results"]
+    end
+  end
+  if query.finished
+  	q += 1
+    puts "Query #{q} / #{book_page_urls_size} finished"
+  end
+end
+
+# Query for tile FR_META
+book_page_urls.each do |url|
+	client.query({"input"=>{"webpage/url"=>url},"connectorGuids"=>["2ee7ccb5-93c0-4eeb-8caa-d6681e40d61e"]}, callback )
+end
+
+puts "Queries dispatched, now waiting for results"
+client.join
+puts "Join completed, all results returned"
+client.disconnect
+
+# Now we can print out the data we got
+puts "All data received! Writing the results to the file."
+
+# Write the results to the file
+File.new('data/fr_meta_results.json', 'w') unless File.exists?('data/fr_meta_results.json')
+File.open('data/fr_meta_results.json', 'w') do |f|
+  f << JSON.pretty_generate(data_rows)
+end
+puts "Done! The results saved to data/fr_meta_results.json."
+
+# Check if every book from every page has been scraped
+json_file = JSON.parse(File.open("data/fr_meta_results.json").read)
+json_page_length = json_file.length
+json_book_per_page_length = json_file[0].length
+
+
+# If all the books are successfully scraped, convert JSON to CSV
+CSV.open("data/fr_meta_results.csv", "w") do |csv|
+  # Write header
+  csv << ["book_page_url", "isbn", "title", "subtitle", "cover_image", "publisher", "pub_date", "author_primary", "author_secondary", "awards", "book_description", "spec_all", "category_all"]
+  
+  # Write rows
+  i = 0
+  n = 0
+
+  (i...json_page_length).each do 
+    (n...json_file[i].length).each do 
+      csv << json_file[i][n].values_at("book_page_url", "isbn", "title", "subtitle", "cover_image", "publisher", "pub_date", "author_primary", "author_secondary", "awards", "book_description", "spec_all", "category_all")
+      n += 1
+    end
+    n = 0
+    i += 1
+  end
+
+end
+puts "The results saved to data/fr_meta_results.csv"
+
+
+# All done!
+puts "와우! 이번 미션은 성공적이었네. 축하하네."
--- to_csv.rb
+++ to_csv.rb
@@ -1,7 +1,31 @@
-require 'rubygems'
-require 'csv'
 require 'json'
+require 'csv'
 
-input = JSON.parse(File.open("data/it_url_results.json").read)
-writer = CSV.open("data/it_url_results.csv", "w")
-writer << input.flatten.to_csv
+
+json_file = JSON.parse(File.open("data/fr_meta_results.json").read)
+json_page_length = json_file.length
+json_book_per_page_length = json_file[0].length
+
+
+# If all the books are successfully scraped, convert JSON to CSV
+
+CSV.open("data/csv_test.csv", "w") do |csv|
+  # Write header
+  csv << ["title", "subtitle", "cover_image", "publisher", "pub_date", "author_primary", "author_secondary", "book_description", "spec_all", "category_all"]
+
+ 
+  # Write rows
+  i = 0
+  n = 0
+
+  (i...json_page_length).each do 
+    (n...json_file[i].length).each do 
+      csv << json_file[i][n].values_at("title", "subtitle", "cover_image", "publisher", "pub_date", "author_primary", "author_secondary", "book_description", "spec_all", "category_all")
+      n += 1
+    end
+    n = 0
+    i += 1
+  end
+
+end
+puts "The results saved to data/csv_test.csv"

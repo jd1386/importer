92b923124fb444f8fd6a04dc4891d319aa59b1fa
Jungdo Lee <lee.jungdo@gmail.com>
2015-04-23 02:03:42 +0900

    enable to parse json format

--- email_validator.rb
+++ email_validator.rb
@@ -7,11 +7,12 @@ email_inputs = ['lee.jungdo@gmail.com', 'jungdo@orights.com', 'heawon.hailey@ori
 
 email_inputs.each do |email|
   puts "Querying #{email}"
-  content = open("https://api.import.io/store/data/5d8a8c21-c2a4-4ee0-90af-1c0a9a9f2d24/_query?input/email=#{email}&_user=3f9ae37e-acfd-44f4-8157-e72adcc5b283&_apikey=3f9ae37e-acfd-44f4-8157-e72adcc5b283%3A93CLLmP2bc%2FxrnSLz8b0BAsVyjebOMqgkxsEz%2FzmojXOtNoPd383KfJLaLXJqaaUzDY8bxZpfM5sDQKi4yUAxg%3D%3D").read
+  content = JSON.parse(open("https://api.import.io/store/data/5d8a8c21-c2a4-4ee0-90af-1c0a9a9f2d24/_query?input/email=#{email}&_user=3f9ae37e-acfd-44f4-8157-e72adcc5b283&_apikey=3f9ae37e-acfd-44f4-8157-e72adcc5b283%3A93CLLmP2bc%2FxrnSLz8b0BAsVyjebOMqgkxsEz%2FzmojXOtNoPd383KfJLaLXJqaaUzDY8bxZpfM5sDQKi4yUAxg%3D%3D").read)
 
   if content
-    puts content
+    puts content["results"]
   else
+    puts "No content"
     next
   end
 

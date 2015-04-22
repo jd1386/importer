e860c850f761b3c9920bcceab431ae581b7b9c68
Jungdo Lee <lee.jungdo@gmail.com>
2015-04-23 01:49:14 +0900

    add email validator

---
+++ email_validator.rb
@@ -0,0 +1,18 @@
+require 'open-uri'
+require 'json'
+require 'csv'
+
+email_inputs = ['lee.jungdo@gmail.com', 'jungdo@orights.com', 'heawon.hailey@orights.com', 'abc@a.c']
+
+
+email_inputs.each do |email|
+  puts "Querying #{email}"
+  content = open("https://api.import.io/store/data/5d8a8c21-c2a4-4ee0-90af-1c0a9a9f2d24/_query?input/email=#{email}&_user=3f9ae37e-acfd-44f4-8157-e72adcc5b283&_apikey=3f9ae37e-acfd-44f4-8157-e72adcc5b283%3A93CLLmP2bc%2FxrnSLz8b0BAsVyjebOMqgkxsEz%2FzmojXOtNoPd383KfJLaLXJqaaUzDY8bxZpfM5sDQKi4yUAxg%3D%3D").read
+
+  if content
+    puts content
+  else
+    next
+  end
+
+end

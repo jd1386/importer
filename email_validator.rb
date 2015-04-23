c7f683a17604bed4bd8a0d9659500e67409f93d4
Jungdo Lee <lee.jungdo@gmail.com>
2015-04-23 18:16:08 +0900

    add exception handling

--- email_validator.rb
+++ email_validator.rb
@@ -1,19 +1,77 @@
 require 'open-uri'
 require 'json'
 require 'csv'
+require 'timeout'
 
-email_inputs = ['lee.jungdo@gmail.com', 'jungdo@orights.com', 'heawon.hailey@orights.com', 'abc@a.c']
+email_inputs = []
+File.readlines('data/email_validator_source.txt').each do |line|
+	email_inputs << line.strip
+end
+puts "Loaded #{email_inputs.size} emails."
 
+
+dataset = []
+q = 1
 
 email_inputs.each do |email|
-  puts "Querying #{email}"
-  content = JSON.parse(open("https://api.import.io/store/data/5d8a8c21-c2a4-4ee0-90af-1c0a9a9f2d24/_query?input/email=#{email}&_user=3f9ae37e-acfd-44f4-8157-e72adcc5b283&_apikey=3f9ae37e-acfd-44f4-8157-e72adcc5b283%3A93CLLmP2bc%2FxrnSLz8b0BAsVyjebOMqgkxsEz%2FzmojXOtNoPd383KfJLaLXJqaaUzDY8bxZpfM5sDQKi4yUAxg%3D%3D").read)
+  puts "Validating #{email} ... #{q} of #{email_inputs.size}"
+  q += 1
 
-  if content
-    puts content["results"]
+	begin
+	  Timeout::timeout(30) do
+	  	@content = JSON.parse(open("https://api.import.io/store/data/5d8a8c21-c2a4-4ee0-90af-1c0a9a9f2d24/_query?input/email=#{email}&_user=3f9ae37e-acfd-44f4-8157-e72adcc5b283&_apikey=3f9ae37e-acfd-44f4-8157-e72adcc5b283%3A93CLLmP2bc%2FxrnSLz8b0BAsVyjebOMqgkxsEz%2FzmojXOtNoPd383KfJLaLXJqaaUzDY8bxZpfM5sDQKi4yUAxg%3D%3D").read)
+	  end
+	rescue Timeout::Error
+		puts "Connection timed out! Take care of #{email} and retry. The results of emails processed so far have been saved to the files."
+		break
+	end  
+
+
+
+  if @content["results"].empty? || @content["results"].nil?
+  	puts "Unknown error. Maybe this email doesn't pass."
+  	temp_hash = Hash.new
+  	temp_hash[:error_message] = "Unknown error"
+  	
+  	@content["results"] << temp_hash
+  	dataset << @content["results"]
   else
-    puts "No content"
-    next
+    dataset << @content["results"]
   end
 
 end
+
+puts "All data received."
+puts dataset.size
+
+# Write to JSON
+File.open('data/email_validator_results.json', 'w') do |f|
+	f << JSON.pretty_generate(dataset)
+end
+
+puts "The results saved to data/email_validator_results.json."
+
+json_file = JSON.parse(File.open("data/email_validator_results.json").read)
+json_page_length = json_file.length
+
+
+# Convert JSON to CSV
+CSV.open("data/email_validator_results.csv", "w") do |csv|
+	# Write header
+	csv << [ "message_3", "error_message" ]
+
+	# Write rows
+
+	i = 0
+	n = 0
+
+	(i...json_page_length).each do 
+		(n...json_file[i].length).each do
+			csv << json_file[i][n].values_at( "message_3", "error_message" )
+			n += 1
+		end
+		n = 0
+		i += 1
+	end
+end
+puts "The results saved to data/email_validator_results.csv."

172e50fc7cd5108e2f6cf73db2021853563672e6
Jungdo Lee <lee.jungdo@gmail.com>
2015-05-01 15:22:57 +0900

    add dotenv to hide user credentials

--- search_grp.rb
+++ search_grp.rb
@@ -3,6 +3,9 @@ require 'uri'
 require 'json'
 require 'csv'
 require 'awesome_print'
+require 'dotenv'
+
+Dotenv.load
 
 
 uri = URI.parse("https://api.import.io/store/data/92a8cd90-fd0c-4eb8-99d3-1d1dc2c7fa42/_query?_user=3f9ae37e-acfd-44f4-8157-e72adcc5b283&_apikey=3f9ae37e-acfd-44f4-8157-e72adcc5b283%3A93CLLmP2bc%2FxrnSLz8b0BAsVyjebOMqgkxsEz%2FzmojXOtNoPd383KfJLaLXJqaaUzDY8bxZpfM5sDQKi4yUAxg%3D%3D ")
@@ -28,8 +31,8 @@ isbn_prefixes.each do |prefix|
             "92a8cd90-fd0c-4eb8-99d3-1d1dc2c7fa42" => {
                 "domainCredentials" => {
                     "grp.isbn-international.org" => {
-                        "username" => "mewons",
-                        "password" => "pica0922"
+                        "username" => ENV['GRP_USER_ID'],
+                        "password" => ENV['GRP_USER_PASSWORD']
                     }
                 }
             }

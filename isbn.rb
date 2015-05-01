a6ef83bc71a4c6075036e7fe10b9e39f838d9833
Jungdo Lee <lee.jungdo@gmail.com>
2015-05-01 15:46:35 +0900

    rename and add original isbn column in the result csv file

--- isbn.rb
+++ isbn.rb
@@ -5,18 +5,18 @@ require 'csv'
 
 @dataset = []
 
-File.readlines('data/isbn_test_source.txt', encoding: 'UTF-8').each do |line|
+File.readlines('data/isbn_source.txt', encoding: 'UTF-8').each do |line|
 	isbn = Lisbn.new(line.rstrip)
 	hashed = Hash.new
 
 	if isbn.valid? 
+		hashed["isbn"] = isbn
 		hashed["validity"] = isbn.valid?
 		hashed["gs1_prefix"] = isbn.parts[0]
 		hashed["country_code"] = isbn.parts[1]
 		hashed["publisher_code"] = isbn.parts[2]
 		hashed["item_number"] = isbn.parts[3]
 		hashed["check_digit"] = isbn.parts[4]
-		#hashed["gs1_country_publisher"] = isbn.parts[0] + '-' + isbn.parts[1] + '-' + isbn.parts[2]
 		hashed["gs1_country_publisher"] = [isbn.parts[0], isbn.parts[1], isbn.parts[2]].join('-')
 
 		puts " #{isbn} \t #{isbn.valid?} \t #{hashed}"
@@ -33,7 +33,7 @@ end
 puts @dataset
 puts @dataset.size
 
-CSV.open('data/isbn_test_results.csv', 'wb') do |csv|
+CSV.open('data/isbn_results.csv', 'wb') do |csv|
 	csv << @dataset.first.keys
 	@dataset.each do |hash|
 		csv << hash.values

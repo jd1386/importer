c70990082e5b1ae53e06f1d6a23b0bb7cc91a6a9
Jungdo Lee <lee.jungdo@gmail.com>
2015-05-04 10:40:30 +0900

    add and finish translate

--- amazon.rb
+++ amazon.rb
@@ -1,10 +1,12 @@
 require 'Vacuum'
 require 'dotenv'
 require 'awesome_print'
+require 'csv'
+require 'json'
 
 Dotenv.load
 
-
+# Configuration
 request = Vacuum.new('ES')
 request.configure(
 	aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
@@ -12,15 +14,88 @@ request.configure(
 	associate_tag: ENV['AWS_ASSOCIATE_TAG']
 )
 
-response = request.item_lookup(
-  query: {
-    'IdType' => 'ISBN',
-    'ItemId' => 9788427200388,
-    'SearchIndex' => 'Books',
-    'ResponseGroup' => 'Large'
-  }
-)
 
-parsed_response = response.to_h
-ap parsed_response["ItemLookupResponse"]["Items"]["Item"]
+# Load isbns to look up
+isbns = []
+
+File.readlines('data/amazon_isbns.txt').each do |line|
+	isbns << line.rstrip
+end
+
+
+# Query each isbn
+isbns.each do |isbn|
+	response = request.item_lookup(
+	  query: {
+	    'IdType' => 'ISBN',
+	    'ItemId' => isbn,
+	    'SearchIndex' => 'Books',
+	    'ResponseGroup' => 'Large',
+	    'MerchantId' => 'Amazon'
+	  }
+	)
+
+	
+
+	parsed_response = response.to_h
+
+
+	# If no results found or error message, print it
+	if parsed_response["ItemLookupResponse"]["Items"]["Request"]["Errors"]
+		@error_message = parsed_response["ItemLookupResponse"]["Items"]["Request"]["Errors"]["Error"]["Message"]
+		ap @error_message
+
+		# Write to CSV
+		CSV.open("data/amazon_results.csv", "a") do |csv|
+			csv << [ isbn, @error_message ]
+		end
+
+	else
+	# Parsing
+	@ean = parsed_response["ItemLookupResponse"]["Items"]["Item"]["ItemAttributes"]["EAN"]
+	@title = parsed_response["ItemLookupResponse"]["Items"]["Item"]["ItemAttributes"]["Title"]
+	@author = parsed_response["ItemLookupResponse"]["Items"]["Item"]["ItemAttributes"]["Author"]
+	
+	@creator_and_role = []
+	# creator == contributors, different from author
+	if parsed_response["ItemLookupResponse"]["Items"]["Item"]["ItemAttributes"]["Creator"].is_a? Hash
+
+		# only one set	
+		@creator_and_role = parsed_response["ItemLookupResponse"]["Items"]["Item"]["ItemAttributes"]["Creator"].values.join(' - ')
+
+	# multiple creators
+	elsif parsed_response["ItemLookupResponse"]["Items"]["Item"]["ItemAttributes"]["Creator"].is_a? Array
+		parsed_response["ItemLookupResponse"]["Items"]["Item"]["ItemAttributes"]["Creator"].each do |hash|
+			@creator_and_role << hash.values.join(' - ')
+			@creator_and_role.join(', ')
+		end
+	
+	
+	end
+
+	
+	@company = parsed_response["ItemLookupResponse"]["Items"]["Item"]["ItemAttributes"]["Publisher"]
+	@pub_date = parsed_response["ItemLookupResponse"]["Items"]["Item"]["ItemAttributes"]["PublicationDate"]
+	@binding = parsed_response["ItemLookupResponse"]["Items"]["Item"]["ItemAttributes"]["Binding"]
+	@number_of_pages = parsed_response["ItemLookupResponse"]["Items"]["Item"]["ItemAttributes"]["NumberOfPages"]
+	@book_page_url = parsed_response["ItemLookupResponse"]["Items"]["Item"]["DetailPageURL"]
+	@cover_image_url = parsed_response["ItemLookupResponse"]["Items"]["Item"]["MediumImage"]["URL"]
+	@number_of_pages = parsed_response["ItemLookupResponse"]["Items"]["Item"]["ItemAttributes"]["NumberOfPages"]
+	@language = parsed_response["ItemLookupResponse"]["Items"]["Item"]["ItemAttributes"]["Languages"]["Language"][0]["Name"]
+
+	
+	puts parsed_response["ItemLookupResponse"]["Items"].to_yaml
+
+
+
+	# Write the results to CSV
+	CSV.open("data/amazon_results.csv", "a") do |csv|
+		csv << [ @ean, @title, @author, @creator_and_role, @company, @pub_date, @binding, @number_of_pages, @language, @book_page_url, @cover_image_url ]
+	end
+
+end
+
+end
+
+puts "Done"
 
--- translate.rb
+++ translate.rb
@@ -1,6 +1,7 @@
 require 'google_fish'
 require 'dotenv'
 require 'titleize'
+require 'retriable'
 require 'csv'
 
 Dotenv.load
@@ -12,14 +13,30 @@ File.readlines('data/translate_source.txt').each do |line|
 	texts_to_translate << line
 end
 
-@processed_count = 0
+processed_count = 0
+text_to_translate_size = texts_to_translate.size
 
-# Loop through and query
-texts_to_translate.each do |text|
-	title_original = text.rstrip
-	title_translated = google.translate(:es, :en, title_original).titleize
+# Open up txt file to write to
+File.open('data/translate_results.txt', 'w') do |f|
+  
+	# Loop through and query
+	texts_to_translate.each do |text|
+		title_original = text.rstrip
+		
+		# Make query
+		# In the case of Timeout Error, retry 3 times.
+		Retriable.retriable on: GoogleFish::Request::ApiError do
+			@title_translated = google.translate(:sv, :en, title_original).titleize
+		end
 
-	puts title_translated
+		# Write to file
+		f.puts(@title_translated)
+
+		# Write to screen
+		processed_count += 1
+		puts "DONE: #{@title_translated} #{processed_count} / #{text_to_translate_size}"
+	end
+
 end
 
-
+puts "All Done!"

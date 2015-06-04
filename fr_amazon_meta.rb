550efaf61c88e2665eebae4c3bc43eb9cf0f79e5
Jungdo Lee <lee.jungdo@gmail.com>
2015-06-04 19:12:30 +0900

    improve the way to handle errors

--- fr_amazon_meta.rb
+++ fr_amazon_meta.rb
@@ -5,6 +5,26 @@ require 'csv'
 require 'awesome_print'
 require 'retriable'
 
+def connect_and_get_response
+  # Connect and receive and parse response. 
+  # In the case of Timeout Error, retry 3 times.
+  Retriable.retriable on: [Timeout::Error, RuntimeError], tries: 4 do
+    Net::HTTP.start(@uri.host, @uri.port, :use_ssl => true) do |http|
+      response = http.request(@req).body.force_encoding('UTF-8')
+      @parsed_response = JSON.parse(response)
+
+      if @parsed_response["results"].empty?
+        puts "Empty!"
+        raise 'Returned with empty results!'
+      elsif @parsed_response["results"].nil?
+        raise 'Nil results!'
+      end
+
+    end
+  end
+
+end
+
 urls = []
 File.readlines('data/fr_amazon_meta_source.txt').each do |line|
 	urls << line.rstrip
@@ -16,14 +36,15 @@ CSV.open("data/fr_amazon_meta_results.csv", "w", encoding: 'UTF-8') do |csv|
 end
 
 
-uri = URI.parse("https://api.import.io/store/data/ef9c29c5-b862-4599-80f6-2d551e1391eb/_query?_user=3f9ae37e-acfd-44f4-8157-e72adcc5b283&_apikey=3f9ae37e-acfd-44f4-8157-e72adcc5b283%3A93CLLmP2bc%2FxrnSLz8b0BAsVyjebOMqgkxsEz%2FzmojXOtNoPd383KfJLaLXJqaaUzDY8bxZpfM5sDQKi4yUAxg%3D%3D")
+@uri = URI.parse("https://api.import.io/store/data/ef9c29c5-b862-4599-80f6-2d551e1391eb/_query?_user=3f9ae37e-acfd-44f4-8157-e72adcc5b283&_apikey=3f9ae37e-acfd-44f4-8157-e72adcc5b283%3A93CLLmP2bc%2FxrnSLz8b0BAsVyjebOMqgkxsEz%2FzmojXOtNoPd383KfJLaLXJqaaUzDY8bxZpfM5sDQKi4yUAxg%3D%3D")
 
 
 @query_count = 0
 @urls_count = urls.size
 
 urls.each do |url|
-	@req = Net::HTTP::Post.new(uri)
+
+	@req = Net::HTTP::Post.new(@uri)
 	@req.body = {
 	        "input" => {
 	            "webpage/url" => url
@@ -41,13 +62,10 @@ urls.each do |url|
 	  }.to_json
 	@req["content-type"] = "application/json"
 
-	# Connect and receive and parse response. 
-  # In the case of Timeout Error, retry 3 times.
-  Retriable.retriable on: Timeout::Error do
-    Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
-	  	response = http.request(@req).body.force_encoding('UTF-8')
-	  	@parsed_response = JSON.parse(response)
-	  end
+	begin
+    connect_and_get_response
+  rescue => e
+    @parsed_response["results"] = []
   end
 
 
@@ -61,10 +79,10 @@ CSV.open("data/fr_amazon_meta_results.csv", "a", encoding: 'UTF-8') do |csv|
 
    # If no results found with the given isbn prefix
   elsif @parsed_response["results"].empty?
-      csv << [
-              @parsed_response["pageUrl"],
-              "Empty"
-      ]
+    csv << [
+            @parsed_response["pageUrl"],
+            "Empty"
+    ]
 
   else
     # Write the results body to csv
@@ -100,3 +118,5 @@ end # end CSV
 
 ap "#{@query_count} / #{@urls_count} DONE: #{@title}"
 end # end urls loop
+
+system('say -v Alex "Work is done come back to work again" ')

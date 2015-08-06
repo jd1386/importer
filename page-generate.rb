a49e81591ef9120892f1f3b32716439b04ac4265
jd1386 <lee.jungdo@gmail.com>
2015-08-06 20:45:54 +0900

    add page generate

---
+++ page-generate.rb
@@ -0,0 +1,18 @@
+
+first_page = "http://www.bookdepository.com/category/2488/Fiction/?page=1&searchRefined=1&ieutf8=%E2%98%A0&searchAddedTerm=&submit=Go&price=&availability=&format=&searchSortBy=pubdate_high_low&searchLang=123"
+#page_scheme = "http://www.bookdepository.com/category/2488/Fiction/best/seller?searchTerm=&addedTerm=&price=&availability=&format=&searchSortBy=pubdate_high_low&searchLang=123&searchTitle=&searchAuthor=&searchPublisher=&searchIsbn=&hasJacket=&seriesId=0&searchDeep=&page=#{page}#pagination"
+
+
+# Write the results to the file
+File.open('data/page-generate-results.txt', 'w') do |f|
+	#f.puts first_page
+	
+	(2001..4000).each do |page|
+		f.puts "http://www.bookdepository.com/category/2488/Fiction/best/seller?searchTerm=&addedTerm=&price=&availability=&format=&searchSortBy=pubdate_high_low&searchLang=123&searchTitle=&searchAuthor=&searchPublisher=&searchIsbn=&hasJacket=&seriesId=0&searchDeep=&page=#{page}#pagination"
+	end
+end
+puts "Done! The results saved to data/page-generate-results.txt"
+
+
+
+

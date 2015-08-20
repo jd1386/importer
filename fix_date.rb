1c9ece161c51c038add155ac1b1ba8af81de5629
jd1386 <lee.jungdo@gmail.com>
2015-08-20 16:52:41 +0900

    update date format

--- fix_date.rb
+++ fix_date.rb
@@ -10,10 +10,8 @@ end
 # Write the results to file
 File.open('data/fix_date_results.txt', 'w') do |f|
 	dates_to_fix.each do |date|
-		
-			#fixed_date = Date.strptime(date, '%Y-%m-%d')
-			#fixed_date = Date.strptime(date, '%b-%y')
-			fixed_date = Date.strptime(date, '%Y.%m.%d') # Netherlands
+	
+		fixed_date = Date.strptime(date, '%d %b %Y')
 		
 		puts fixed_date
 		f.puts fixed_date

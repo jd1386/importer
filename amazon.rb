1f87a023450fa5a5914f7475c80d33c16ed59ad3
jd1386 <lee.jungdo@gmail.com>
2015-12-17 16:23:15 +0900

    add threads

--- amazon.rb
+++ amazon.rb
@@ -83,7 +83,7 @@ end
 Dotenv.load
 
 # Configuration
-@request = Vacuum.new('US')
+@request = Vacuum.new('FR')
 @request.configure(
 	aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
 	aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
--- email_validator.rb
+++ email_validator.rb
@@ -2,7 +2,69 @@ require 'open-uri'
 require 'json'
 require 'csv'
 require 'timeout'
+require 'colorize'
+require 'awesome_print'
+require 'valid_email'
 
+
+def query(email)
+	begin
+		JSON.parse(open("https://api.import.io/store/data/5d8a8c21-c2a4-4ee0-90af-1c0a9a9f2d24/_query?input/email=#{email}&_user=3f9ae37e-acfd-44f4-8157-e72adcc5b283&_apikey=3f9ae37e-acfd-44f4-8157-e72adcc5b283%3A93CLLmP2bc%2FxrnSLz8b0BAsVyjebOMqgkxsEz%2FzmojXOtNoPd383KfJLaLXJqaaUzDY8bxZpfM5sDQKi4yUAxg%3D%3D").read)
+	rescue OpenURI::HTTPError => error
+		response = error.io
+		puts response.status.join(', ').red
+	end
+end
+
+def parse(content)
+	message = content["results"][0]
+
+	if message != nil
+		if message.has_key?("message_3")
+			puts message["message_3"].blue
+			@api_message = message["message_3"]
+		else
+			puts message["error_message"].red
+			@api_message = message["error_message"]
+		end
+	else
+		puts 'Unknown'.purple
+		@api_message = "Unknown"
+	end
+
+end
+
+def write_to_csv(email, message, last_checked_at)
+  CSV.open("data/email_validator_results.csv", "a") do |csv|
+		csv << [ email, message, last_checked_at ]
+	end
+end
+
+def main_job(email, index)
+	begin
+		Timeout::timeout(30) do
+			# Query to API
+			@content = query(email)
+			sleep 1
+
+			if @content && @content["results"][0]
+				# Parse returned results
+				parse(@content)
+				@container << [email, @api_message, Time.now, index]
+			else
+				# 503 error
+				puts "503 Error:\t#{email}.".colorize(background: :red)
+				@container << [email, 'Server unavailable', Time.now, index]
+			end
+		end
+	rescue Timeout::Error
+		# There's no content
+		puts "Server timeout:\t#{email}.".colorize(background: :red)
+		@container << [email, 'Server timeout', Time.now, index]
+	end
+end
+
+@container = []
 email_inputs = []
 File.readlines('data/email_validator_source.txt').each do |line|
 	email_inputs << line.strip
@@ -13,64 +75,28 @@ puts "Loaded #{email_inputs.size} emails."
 dataset = []
 q = 1
 
-email_inputs.each do |email|
-  puts "Validating #{email} ... #{q} of #{email_inputs.size}"
-  q += 1
+email_inputs.each_with_index do |email, index|
+  puts "Validating #{email} ... #{index + 1} of #{email_inputs.size}"
 
-	begin
-	  Timeout::timeout(30) do
-	  	@content = JSON.parse(open("https://api.import.io/store/data/5d8a8c21-c2a4-4ee0-90af-1c0a9a9f2d24/_query?input/email=#{email}&_user=3f9ae37e-acfd-44f4-8157-e72adcc5b283&_apikey=3f9ae37e-acfd-44f4-8157-e72adcc5b283%3A93CLLmP2bc%2FxrnSLz8b0BAsVyjebOMqgkxsEz%2FzmojXOtNoPd383KfJLaLXJqaaUzDY8bxZpfM5sDQKi4yUAxg%3D%3D").read)
-	  end
-	rescue Timeout::Error
-		# There's no @content
-
-		puts "CONNECTION TIMED OUT:\t#{email}."
-		break
-	end  
-
-
-
-  if @content["results"].empty? || @content["results"].nil?
-  	puts "OK"
-  	temp_hash = Hash.new
-  	temp_hash[:message_3] = "E-mail address is valid"
-  	
-  	@content["results"] << temp_hash
-  	dataset << @content["results"]
-  else
-    dataset << @content["results"]
-  end
+	@threads = []
+	@threads << Thread.new {
+		main_job(email, index)
+	 }
 
 end
 
-puts "All data received."
-puts dataset.size
-
-# Write to JSON
-File.open('data/email_validator_results.json', 'w') do |f|
-	f << JSON.pretty_generate(dataset)
+# Join threads
+Thread.list.each do |t|
+	t.join if t != Thread.current
 end
 
-puts "The results saved to data/email_validator_results.json."
 
-json_file = JSON.parse(File.open("data/email_validator_results.json").read)
-json_page_length = json_file.length
+# Sort threads by index value, otherwise it will mess up order
+@container.sort_by! { |e| e[3] }
 
-
-# Convert JSON to CSV
-CSV.open("data/email_validator_results.csv", "w") do |csv|
-# Write rows
-
-	i = 0
-	n = 0
-
-	(i...json_page_length).each do 
-		(n...json_file[i].length).each do
-			csv << json_file[i][n].values_at( "message_3" )
-			n += 1
-		end
-		n = 0
-		i += 1
-	end
+# Save results to csv
+@container.each do |c|
+	write_to_csv(c[0], c[1], c[2])
 end
-puts "The results saved to data/email_validator_results.csv."
+
+puts "*=*=*=*=*=*=*=", "The results saved to data/email_validator_results.csv."

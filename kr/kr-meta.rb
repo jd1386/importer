1ee35cc4a8ea21c840919afcf83cded0de02c7a4
Jungdo Lee <lee.jungdo@gmail.com>
2016-02-22 23:41:06 +0900

    initial

--- kr/kr-meta.rb
+++ kr/kr-meta.rb
@@ -8,30 +8,83 @@ COMMENT
 
 require 'mechanize'
 require 'awesome_print'
+require 'csv'
+require 'colorize'
+
 
 def clean_meta_1(meta_1)
 	splitted = meta_1.split(" | ")
 	size = splitted.size
-	
-	if splitted[size - 1].include? '원제'
-		original_title = splitted[size - 1]
-		pub_date = splitted[size - 2] 
-		publisher = splitted[size - 3]
-		authors = splitted[0..size-4].join(', ')
-	else
-		original_title = ''
-		pub_date = splitted[size - 1] 
-		publisher = splitted[size - 2]
-		authors = splitted[0..size-3].join(', ')
-	end
-	
 
-	return authors, publisher, pub_date, original_title
+	if splitted.any?
+		if splitted[size - 1].include? '원제'
+			@original_title = splitted[size - 1].gsub('원제 ', '')
+			@pub_date = splitted[size - 2]
+			@publisher = splitted[size - 3]
+			@authors = splitted[0..size-4].join(', ')
+		else
+			@original_title = ''
+			@pub_date = splitted[size - 1]
+			@publisher = splitted[size - 2]
+			@authors = splitted[0..size-3].join(', ')
+		end
+	else
+		puts "ERROR FOUND! - Meta 1".red
+		return nil
+	end
+
+
+	return @authors, @publisher, @pub_date, @original_title
 end
 
 def clean_meta_2(meta_2)
-	meta_2.split(" | ")
+	splitted = meta_2.split(" | ")
+	size = splitted.size
+
+	if splitted.any?
+		if size == 4
+			@binding = ''
+		elsif size == 5
+			@binding = splitted[0]
+		end
+		@pages = splitted[size - 4]
+		@dimension = splitted[size - 3]
+		@weight = splitted[size - 2]
+		@isbn = splitted[size - 1].gsub("ISBN : ", "")
+	else
+		puts "ERROR FOUND! - Meta 2".red
+		return nil
+	end
 end
+
+def results_returned?
+	!clean_meta_1(@meta_1).nil? && !clean_meta_2(@meta_2).nil?
+
+end
+
+def write_to_csv(results)
+	CSV.open("meta_results.csv", "a") do |csv|
+		if results == 'error'
+			csv << ['ERROR']
+		else
+			csv << [
+				@current_page,
+				@isbn,
+				@title_and_subtitle,
+				@authors,
+				@publisher,
+				@pub_date,
+				@original_title,
+				@cover_image,
+				@series,
+				@binding,
+				@dimension,
+				@weight
+			]
+		end
+	end
+end
+
 
 pages = []
 links = []
@@ -41,7 +94,7 @@ File.readlines('meta_source.txt', encoding: 'UTF-8').each do |line|
 end
 
 pages.each_with_index do |page, index|
-	print "Page #{index + 1} ... "
+	puts "Page #{index + 1} ... "
 
 	agent = Mechanize.new
 	agent.user_agent_alias = 'Windows Edge'
@@ -49,32 +102,41 @@ pages.each_with_index do |page, index|
 	page.encoding = "euc-kr"
 
 
-	title_subtitle_and_series = page.search("td.pwrap_bgtit").search("table[1]")[0].text.strip
-	title_and_subtitle = title_subtitle_and_series.split(" l ")[0]
-	series = title_subtitle_and_series.split(" l ")[1]
+	@current_page = page.uri.to_s
+	title_subtitle_and_series = page.search("td.pwrap_bgtit").search("table[1]").text.strip
+	@title_and_subtitle = title_subtitle_and_series.split(" l ")[0]
+	@series = title_subtitle_and_series.split(" l ")[1]
 
-	puts "#{title_and_subtitle} #{(series)}"
 
-	meta_1 = page.search("td.pwrap_bgtit/table[2]").search("tr/td[1]").text.strip
-	meta_1_cleaned = clean_meta_1(meta_1)
-	ap meta_1_cleaned
+	@meta_1 = page.search("td.pwrap_bgtit/table[2]").search("tr/td[1]").text.strip
+	meta_1_cleaned = clean_meta_1(@meta_1)
+
+	if meta_1_cleaned.nil?
+		next
+	end
+
 
 	# meta_2 includes authors, publisher, and pub_date
-	meta_2 = page.search("div.p_goodstd03").text.strip
-	meta_2_cleaned = clean_meta_2(meta_2)
-	puts "\t#{meta_2_cleaned}"
+	@meta_2 = page.search("div.p_goodstd03").first.text.strip
+	meta_2_cleaned = clean_meta_2(@meta_2)
 
-	#categories = page.search("div.p_categorize").text.strip
-	#puts "\t#{categories}"
+	if meta_2_cleaned.nil?
+		next
+	end
 
-	
-	
+	@cover_image = page.image_with(src: /cover/)
 
-	#print "DONE\n"
+
+	puts @current_page, @title_and_subtitle, @series, @authors, @publisher, @pub_date, @original_title
+	puts @binding, @dimension, @weight, @isbn
+	puts @cover_image
+	puts "========="
+
+	if results_returned?
+		write_to_csv('success')
+	else
+		write_to_csv('error')
+	end
+
 end
 
-
-
-# CSV.open("data/amazon_results.csv", "a") do |csv|
-# 	csv << [  ]
-# end
--- kr/page-gen.rb
+++ kr/page-gen.rb
@@ -1,8 +1,8 @@
 # Write the results to the file
 File.open('/Users/jungdolee/projects/importer/data/page-generate-results.txt', 'w') do |f|
 	#f.puts first_page
-	
-	(1..20).each do |page|
+
+	(1..100).each do |page|
 		f.puts "http://www.aladin.co.kr/shop/wbrowse.aspx?BrowseTarget=List&ViewRowsCount=25&ViewType=Detail&PublishMonth=0&SortOrder=5&page=#{page}&Stockstatus=1&PublishDay=84&CID=1108&CustReviewRankStart=&CustReviewRankEnd=&CustReviewCountStart=&CustReviewCountEnd=&PriceFilterMin=&PriceFilterMax="
 	end
 end
